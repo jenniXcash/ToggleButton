373fbd9308b5bfd642105f86b7eb0c23a7b02085
jenDoeMoldovan <michalevic@gmail.com>
2021-10-26 10:55:27 +0300

    final

--- src/Components/ToggleButton/ToggleButton.js
+++ src/Components/ToggleButton/ToggleButton.js
@@ -2,18 +2,22 @@ import "./ToggleButton.css";
 import { useState } from "react";
 
 export default function ToggleButton() {
-  const [text, setVisibility] = useState(
-    "Leprosy will take control and bring you to your death"
-  );
+  const [text, setVisibility] = useState("visible");
+  let visibility = { visibility: text };
   function showHide() {
-    text
-      ? setVisibility("")
-      : setVisibility("Leprosy will take control and bring you to your death");
+    if (text === "visible") {
+      setVisibility("hidden");
+    } else {
+      setVisibility("visible");
+    }
+    console.log(visibility);
   }
   return (
     <>
+      <p style={visibility}>
+        Leprosy will take control and bring you to your death
+      </p>
       <button onClick={showHide}> Delete/Rewrite</button>
-      {text}
     </>
   );
 }
